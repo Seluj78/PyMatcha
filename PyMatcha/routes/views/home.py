a64eea93beade8552abec9399ef534057a08501f
Nicolas BENY <nbeny@e1r8p8.42.fr>
2018-11-26 13:37:04 +0100

    add boootstrap in static + some example templates + env.sh for simplicity execution

--- PyMatcha/routes/views/home.py
+++ PyMatcha/routes/views/home.py
@@ -19,6 +19,8 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
+from flask import render_template
+
 from flask import Blueprint
 
 home_bp = Blueprint('home', __name__)
---
+++ env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export FLASK_ENV='app.py'
+export FLASK_DEBUG=1
+export FLASK_SECRET_KEY="SecretKeyDev=D"
+flask run
