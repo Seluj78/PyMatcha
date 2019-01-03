a04ce59beb2facc5f6642719e29cadc7c438f651
Jules Lasne (jlasne) <jlasne@student.42.fr>
2019-01-03 11:09:08 +0100

    Added flask-admin (#22)
    * Added flask-admin
    
    * Added black check in travis config
    
    * Changed travis command for black + black changed two files
    
    * Added black configuration file
    
    * Removed 3.5 from build and test process
    
    * Update __init__.py

--- PyMatcha/__init__.py
+++ PyMatcha/__init__.py
@@ -24,20 +24,29 @@ import os
 
 from flask import Flask
 
+from flask_admin import Admin
+
 from PyMatcha.routes.views.home import home_bp
 
 if os.environ.get("FLASK_ENV", None) == "dev":
-    os.environ['FLASK_DEBUG'] = '1'
-    os.environ['FLASK_SECRET_KEY'] = "ThisIsADevelopmentKey"
+    os.environ["FLASK_DEBUG"] = "1"
+    os.environ["FLASK_SECRET_KEY"] = "ThisIsADevelopmentKey"
 
 if "FLASK_DEBUG" not in os.environ:
-    raise EnvironmentError("Debug bool is not set in the server's environment. Please fix and restart the server.")
+    raise EnvironmentError(
+        "FLASK_DEBUT is not set in the server's environment. Please fix and restart the server."
+    )
 
 if "FLASK_SECRET_KEY" not in os.environ:
-    raise EnvironmentError("Secret key is not set in the server's environment. Please fix and restart the server.")
+    raise EnvironmentError(
+        "FLASK_SECRET_KEY is not set in the server's environment. Please fix and restart the server."
+    )
 
 application = Flask(__name__)
 application.debug = os.environ.get("FLASK_DEBUG", 1)
 application.secret_key = os.environ.get("FLASK_SECRET_KEY", "ThisIsADevelopmentKey")
+
+application.config["FLASK_ADMIN_SWATCH"] = "simplex"
+admin = Admin(application, name="PyMatcha", template_mode="bootstrap4")
 
 application.register_blueprint(home_bp)
--- PyMatcha/routes/views/home.py
+++ PyMatcha/routes/views/home.py
@@ -21,9 +21,9 @@
 
 from flask import Blueprint
 
-home_bp = Blueprint('home', __name__)
+home_bp = Blueprint("home", __name__)
 
 
-@home_bp.route('/')
+@home_bp.route("/")
 def home_view():
     return "Hello World !"
