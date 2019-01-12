dca5810ad12cdfec85dec43de567b941ce50f701
nbeny <nbeny@student.42.fr>
2019-01-12 01:27:16 +0100

    update flask_wtf.form to Flask_wtf.FlaskForm

--- PyMatcha/forms/chat.py
+++ PyMatcha/forms/chat.py
@@ -19,11 +19,11 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms.fields import StringField, SubmitField
 from wtforms.validators import DataRequired
 
-class ChatForm(Form):
+class ChatForm(FlaskForm):
     """Accepts a nickname and a room."""
     name = StringField('Name', validators=[DataRequired()])
     room = StringField('Room', validators=[DataRequired()])
