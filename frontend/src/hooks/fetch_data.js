2100504d88cf40abb93aebb76c236954ebc17de0
gmorer <gmorer@pm.me>
2020-02-11 23:15:56 +0100

    fix return name

--- frontend/src/hooks/fetch_data.js
+++ frontend/src/hooks/fetch_data.js
@@ -2,13 +2,13 @@ import { useState, useEffect } from 'react';
 import { apiCall } from '../utils';
 
 const onLoad = async (url, options, setState) => {
-	setState({ loading: true, error: undefined, data: undefined})
+	setState({ loading: true, error: undefined, result: undefined})
 	try {
-		const data = await apiCall({ uri: url, ...options });
-		setState({ loading: false, error: undefined, data });
+		const result = await apiCall({ uri: url, ...options });
+		setState({ loading: false, error: undefined, result });
 	} catch (e) {
 		console.error(e)
-		setState({ loading: false, error: e.message, data: undefined});
+		setState({ loading: false, error: e.message, result: undefined});
 	}
 }
 
@@ -17,7 +17,7 @@ const useFetchData = (url, options = {}) => {
 	const [state, setState] = useState({
 		loading: true,
 		error: undefined,
-		data: undefined
+		result: undefined
 	});
 
 	useEffect(() => {
--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,6 +1,6 @@
 import request from 'request-promise';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
 
 const apiCall = options => {
 	// TODO add token in the header
