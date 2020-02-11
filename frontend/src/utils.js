d3b5769cbd704b9627ca32e2b0436df6d7590f1e
Jules Lasne (jlasne) <jlasne@student.42.fr>
2020-02-11 23:44:40 +0100

    Revert "Hook and function to communicate with the api (#44)"
    This reverts commit d041aee901f63af24d972398744e7c43e843b38a.

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,18 +1,5 @@
-import request from 'request-promise';
-
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
-
-const apiCall = options => {
-	// TODO add token in the header
-	options.json = true;
-	options.method = options.method || 'GET';
-	options.uri = `${API_URL}/${options.uri}`;
-	return request(options);
-}
-
 const getToken = () => sessionStorage.getItem('token')
 
 export {
-	getToken,
-	apiCall
+	getToken
 }
