bbcb852aaab32664222a65eb03125292766273a1
Jules Lasne (jlasne) <jlasne@student.42.fr>
2020-02-20 14:16:33 +0100

    Added Errors and Successes (#66)
    * Added sexual orientation
    
    * Added frontend and www to black ignore
    
    * Added tags
    
    * Moved create user table function in separate function
    
    * Added errors and Success

---
+++ backend/PyMatcha/success/__init__.py
@@ -0,0 +1,24 @@
+"""
+    PyMatcha - A Python Dating Website
+    Copyright (C) 2018-2019 jlasne/gmorer
+    <jlasne@student.42.fr> - <gmorer@student.42.fr>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from PyMatcha.success.created import SuccessCreated
+from PyMatcha.success.deleted import SuccessDeleted
+from PyMatcha.success.success import Success, SuccessOutput, SuccessOutputMessage
+
+__all__ = ["SuccessDeleted", "SuccessCreated", "Success", "SuccessOutput", "SuccessOutputMessage"]
