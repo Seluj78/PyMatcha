8066524a3951a078f702a48db7cf6795f1befc54
Jules Lasne (jlasne - seluj78) <jules.lasne@gmail.com>
2020-05-29 12:47:15 +0200

    Added edit profile route

--- backend/PyMatcha/routes/api/profile.py
+++ backend/PyMatcha/routes/api/profile.py
@@ -23,6 +23,7 @@ import PyMatcha.models.user as user
 from flask import Blueprint
 from flask import request
 from PyMatcha.errors import BadRequestError
+from PyMatcha.errors import NotFoundError
 from PyMatcha.models.tag import Tag
 from PyMatcha.success import Success
 from PyMatcha.utils.decorators import validate_params
@@ -33,6 +34,16 @@ get_user = user.get_user
 profile_bp = Blueprint("profile", __name__)
 
 REQUIRED_PARAMS_COMPLETE_PROFILE = {"gender": str, "birthdate": int, "orientation": str, "bio": str, "tags": list}
+REQUIRED_PARAMS_EDIT_PROFILE = {
+    "first_name": str,
+    "last_name": str,
+    "username": str,
+    "bio": str,
+    "gender": str,
+    "orientation": str,
+    "birthdate": int,
+    "tags": list,
+}
 
 
 @profile_bp.route("/profile/complete", methods=["POST"])
@@ -51,12 +62,6 @@ def complete_profile():
     gender = data["gender"]
     birthdate = data["birthdate"]
 
-    if orientation not in ["heterosexual", "homosexual", "bisexual", "other"]:
-        raise BadRequestError("Orientation must be heterosexual, homosexual, bisexual or other", "Try again")
-
-    if gender not in ["male", "female", "other"]:
-        raise BadRequestError("Gender must be male, female or other", "Try again")
-
     for tag in tags:
         Tag.create(name=tag, user_id=current_user.id)
 
@@ -67,3 +72,45 @@ def complete_profile():
     current_user.birthdate = datetime.date.fromtimestamp(int(birthdate))
     current_user.save()
     return Success("Profile completed !")
+
+
+@profile_bp.route("/profile/edit", methods=["PUT"])
+@fjwt.jwt_required
+@validate_params(REQUIRED_PARAMS_EDIT_PROFILE)
+def edit_profile():
+    current_user = fjwt.current_user
+    if not current_user.is_profile_completed:
+        raise BadRequestError("The user has not completed his profile", "Complete your profile and try again")
+    data = request.get_json()
+    first_name = data["first_name"]
+    last_name = data["last_name"]
+    username = data["username"]
+    bio = data["bio"]
+    gender = data["gender"]
+    orientation = data["orientation"]
+    birthdate = data["birthdate"]
+
+    try:
+        get_user(username)
+    except NotFoundError:
+        pass
+    else:
+        raise BadRequestError("Username taken", "Try again")
+
+    if orientation not in ["heterosexual", "homosexual", "bisexual", "other"]:
+        raise BadRequestError("Orientation must be heterosexual, homosexual, bisexual or other", "Try again")
+
+    if gender not in ["male", "female", "other"]:
+        raise BadRequestError("Gender must be male, female or other", "Try again")
+
+    birthdate = datetime.date.fromtimestamp(birthdate)
+
+    current_user.first_name = first_name
+    current_user.last_name = last_name
+    current_user.username = username
+    current_user.bio = bio
+    current_user.gender = gender
+    current_user.orientation = orientation
+    current_user.birthdate = birthdate
+    current_user.save()
+    return Success("User successfully modified !")
