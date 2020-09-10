2a17c7b0f4d6e7f724372bc70870a8a02f8b1edc
Jules Lasne (jlasne - seluj78) <jules.lasne@gmail.com>
2020-09-10 15:39:12 +0200

    Updated heat score calculation

--- backend/PyMatcha/utils/tasks.py
+++ backend/PyMatcha/utils/tasks.py
@@ -1,36 +1,44 @@
 import datetime
+import json
 import logging
+from math import ceil
 
 from PyMatcha import celery
 from PyMatcha import redis
+from PyMatcha.models.message import Message
 from PyMatcha.models.user import User
+from PyMatcha.utils.match_score import _get_age_diff
+from PyMatcha.utils.match_score import _get_common_tags
+from PyMatcha.utils.match_score import _get_distance
+from PyMatcha.utils.match_score import _get_inverted_gender
 
 
 @celery.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(60, update_offline_users.s(), name="Update online users every minute")
-    sender.add_periodic_task(3600, update_popularity_scores.s(), name="Update popularity scores every minute")
+    sender.add_periodic_task(3600, update_heat_scores.s(), name="Update heat scores every hour")
 
 
 @celery.task
-def update_popularity_scores():
-    # TODO: If user is Jules or Guilhem: +100 score
-
+def update_heat_scores():
     for user in User.select_all():
         likes_received = len(user.get_likes_received())
         reports_received = len(user.get_reports_received())
         views = len(user.get_views())
+        matches = len(user.get_matches())
+        messages = len(Message.get_multi(to_id=user.id))
 
-        # TODO: Add matches
-        # TODO: Add messages
-
-        points = 30
-        points += likes_received * 2
+        score = 30
+        if user.username == "seluj78" or user.username == "tet":
+            score += 100
+        score += likes_received * 2
+        score += matches * 4
         # TODO: Superlike received = 5 pts
-        points -= reports_received * 10
-        points += views
-        # TODO: remove 5 pts per week of inactivity
-        user.heat_score = points
+        score -= reports_received * 10
+        score += views
+        score += ceil(messages / 5)
+        # TODO: remove 10 pts per week of inactivity
+        user.heat_score = score
         user.save()
         return f"Updated heat score for user {user.id}: {user.heat_score}."
 
