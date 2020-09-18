a5d1fa4bf11be83086079e0065cb9d29c881fb96
Jules Lasne (jlasne - seluj78) <jules.lasne@gmail.com>
2020-09-18 12:20:39 +0200

    Fixed match score again

--- backend/PyMatcha/utils/match_score.py
+++ backend/PyMatcha/utils/match_score.py
@@ -1,4 +1,3 @@
-import itertools
 from typing import List
 from typing import Optional
 
@@ -107,7 +106,11 @@ def _get_gender_query(orientation, gender):
             q4 = User.get_multis(orientation="homosexual", gender="male")
         q5 = User.get_multis(orientation="other", gender="male")
         q6 = User.get_multis(orientation="other", gender="female")
-        return list(set(list(itertools.chain(q1, q2, q3, q4, q5, q6))))
+        final = []
+        for q in [q1, q2, q3, q4, q5, q6]:
+            if q:
+                final.extend(q)
+        return list(set(final))
     elif orientation == "other":
         q1 = User.get_multis(orientation=orientation, gender="female")
         q3 = User.get_multis(orientation=orientation, gender="male")
@@ -121,6 +124,10 @@ def _get_gender_query(orientation, gender):
         if gender == "male":
             q4 = User.get_multis(orientation="homosexual", gender="male")
             q6 = User.get_multis(orientation="heterosexual", gender="female")
-        return list(set(list(itertools.chain(q1, q2, q3, q4, q5, q6))))
+        final = []
+        for q in [q1, q2, q3, q4, q5, q6]:
+            if q:
+                final.extend(q)
+        return list(set(final))
     else:
         raise ValueError("No match found for genre. This should not happen")
