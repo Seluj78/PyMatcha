228bfbe301ec06dedfc9dbf21e29ada01ed6aab8
supalarry <lauris.skraucis@gmail.com>
2020-09-18 11:21:52 +0300

    invalid password refresh token or already reset password page

---
+++ frontend/src/views/auth/ResetPasswordError.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- eslint-disable max-len -->
+  <div class="auth-container">
+    <div class="auth-sub-container">
+      <div class="auth-sub-container-content">
+        <img src="../../assets/auth/error.png" class="h-12">
+        <h1 class="auth-sub-container-content-heading">Something is wrong</h1>
+        <h1 class="text-sm text-gray-matcha text-center">Either password has been reset already or this request is invalid</h1>
+        <router-link to="/accounts/password/forgot" class="auth-sub-container-content-link mt-4">Request new password</router-link>
+      </div>
+    </div>
+    <div class="auth-sub-container-thinner mt-4">
+      <div class="auth-sub-container-content">
+        <router-link to="/accounts/signin" class="auth-sub-container-content-link">Sign in</router-link>
+      </div>
+    </div>
+  </div>
+</template>
