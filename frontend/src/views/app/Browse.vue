ce6ef8bb370d8e05c135910ee2ffa5ad246fb57c
supalarry <lauris.skraucis@gmail.com>
2020-09-21 22:36:14 +0300

    sample pages to redirect user based on profile completion

---
+++ frontend/src/views/app/Browse.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- eslint-disable max-len -->
+  <div>
+    <h1>browse</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+};
+
+</script>
---
+++ frontend/src/views/app/Onboarding.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- eslint-disable max-len -->
+  <div>
+    <h1>onboarding</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+};
+
+</script>
