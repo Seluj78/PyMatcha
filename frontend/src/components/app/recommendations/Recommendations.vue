4023367069facfd33dfb6d6c88cb83979d18834b
supalarry <lauris.skraucis@gmail.com>
2020-09-26 23:47:07 +0300

    browsing page

---
+++ frontend/src/components/app/recommendations/Recommendations.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- eslint-disable max-len -->
+  <section>
+    <h1 class="text-5xl mb-4 text-center sm:text-left onboarding-sub-container-content-heading">{{title}}</h1>
+    <div class="flex flex-wrap items-center justify-start">
+    <RecommendationCard
+      v-for="(recommendation, index) in recommendations" :key="index"
+      v-bind:recommendation="recommendation" v-bind:index="index + 1"></RecommendationCard>
+    </div>
+  </section>
+</template>
+
+<script>
+import RecommendationCard from '@/components/app/recommendations/RecommendationCard.vue';
+
+export default {
+  components: {
+    RecommendationCard,
+  },
+  props: ['title', 'recommendations'],
+};
+</script>
