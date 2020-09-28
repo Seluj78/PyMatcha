b5100574510186360e83b5613721172e3fb5ad45
Larry <lauris.skraucis@gmail.com>
2020-09-28 22:00:10 +0300

    analyse recommendations to get filter starting values

--- frontend/src/views/app/Browse.vue
+++ frontend/src/views/app/Browse.vue
@@ -4,13 +4,16 @@
     <NavBar v-bind:currentRoute="'Browse'"></NavBar>
     <section class="mx-auto">
       <Recommendations
+        v-if="recommendationsAnalysisDone"
         v-bind:title="'Potential matches'"
-        v-bind:recommendations="recommendations"></Recommendations>
+        v-bind:recommendations="recommendations"
+        v-bind:recommendationsAnalysis="recommendationsAnalysis"></Recommendations>
     </section>
   </div>
 </template>
 
 <script>
+/* eslint-disable max-len */
 import NavBar from '@/components/shared/NavBar.vue';
 import Recommendations from '@/components/app/recommendations/Recommendations.vue';
 
@@ -22,6 +25,22 @@ export default {
   },
   data: () => ({
     recommendations: [],
+    recommendationsAnalysis: {
+      age: {
+        min: null,
+        max: null,
+      },
+      distance: {
+        min: null,
+        max: null,
+      },
+      popularity: {
+        min: null,
+        max: null,
+      },
+      uniqueInterests: [],
+    },
+    recommendationsAnalysisDone: false,
   }),
   async created() {
     if (this.recommendationsFromSettingUp) {
@@ -30,6 +49,33 @@ export default {
       const recommendationsRequest = await this.$http.get('/recommendations');
       this.recommendations = recommendationsRequest.data.recommendations;
     }
+    for (let i = 0; i < this.recommendations.length; i += 1) {
+      if (this.recommendationsAnalysis.age.min === null || this.recommendations[i].age < this.recommendationsAnalysis.age.min) {
+        this.recommendationsAnalysis.age.min = this.recommendations[i].age;
+      }
+      if (this.recommendationsAnalysis.age.max === null || this.recommendations[i].age > this.recommendationsAnalysis.age.max) {
+        this.recommendationsAnalysis.age.max = this.recommendations[i].age;
+      }
+      if (this.recommendationsAnalysis.distance.min === null || Math.floor(this.recommendations[i].distance) < this.recommendationsAnalysis.distance.min) {
+        this.recommendationsAnalysis.distance.min = Math.floor(this.recommendations[i].distance);
+      }
+      if (this.recommendationsAnalysis.distance.max === null || Math.floor(this.recommendations[i].distance) > this.recommendationsAnalysis.distance.max) {
+        this.recommendationsAnalysis.distance.max = Math.floor(this.recommendations[i].distance);
+      }
+      if (this.recommendationsAnalysis.popularity.min === null || this.recommendations[i].heat_score < this.recommendationsAnalysis.popularity.min) {
+        this.recommendationsAnalysis.popularity.min = this.recommendations[i].heat_score;
+      }
+      if (this.recommendationsAnalysis.popularity.max === null || this.recommendations[i].heat_score > this.recommendationsAnalysis.popularity.max) {
+        this.recommendationsAnalysis.popularity.max = this.recommendations[i].heat_score;
+      }
+      const interests = this.recommendations[i].tags;
+      for (let j = 0; j < interests.length; j += 1) {
+        if (this.recommendationsAnalysis.uniqueInterests.indexOf(interests[j].name) === -1) {
+          this.recommendationsAnalysis.uniqueInterests.push(interests[j].name);
+        }
+      }
+    }
+    this.recommendationsAnalysisDone = true;
   },
 };
 
