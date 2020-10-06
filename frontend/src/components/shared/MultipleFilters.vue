6761c88eb1731911300b479714b18b319065ebbb
Larry <lauris.skraucis@gmail.com>
2020-10-06 11:10:37 +0300

    add optional description for filter

--- frontend/src/components/shared/MultipleFilters.vue
+++ frontend/src/components/shared/MultipleFilters.vue
@@ -1,7 +1,8 @@
 <template>
   <!-- eslint-disable max-len -->
   <div class='mx-auto max-w-md mb-4'>
-    <h1 class="text-xl lg text-purple-matcha capitalize text-left mb-4">{{ name }}</h1>
+    <h1 class="text-xl lg text-purple-matcha capitalize text-left">{{ name }}</h1>
+    <h1 class="text-md text-left opacity-50 mb-4">{{ info }}</h1>
     <div class="h-48 w-auto overflow-scroll rounded-md border border-gray-300 bg-white">
       <h1 v-for="(option, index) in options" :key="option + index + option"
           v-bind:class="{'sort-dropdown-option': true, 'text-lg': true, 'border-b': index !== options.length - 1, 'capitalize': true,
@@ -15,7 +16,7 @@
 
 <script>
 export default {
-  props: ['options', 'name', 'position'],
+  props: ['options', 'name', 'info', 'position'],
   data: () => ({
     closed: true,
     selectedFilters: [],
