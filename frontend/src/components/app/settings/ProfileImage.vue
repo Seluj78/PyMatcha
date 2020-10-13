a4b6e794763a7126bf8ee01bf0799ae1f04e0e49
Larry <lauris.skraucis@gmail.com>
2020-10-13 21:41:30 +0300

    image button color change

--- frontend/src/components/app/settings/ProfileImage.vue
+++ frontend/src/components/app/settings/ProfileImage.vue
@@ -6,8 +6,8 @@
       <div class="absolute top-0 left-0 z-20 m-5">
         <img src="../../../assets/dots.png" tabindex="2" @focusout="showImageSettings = false" v-on:click="showImageSettings = !showImageSettings " class="outline-none cursor-pointer h-3">
         <div v-if="showImageSettings" class="z-30 mt-4">
-          <h1 class="text-white-matcha font-bold text-center bg-red-500 px-4 py-2 cursor-pointer rounded-md" v-on:click="deleteImage(image.id)">Delete</h1>
-          <h1 class="text-white-matcha font-bold text-center bg-green-500 px-4 py-2 mt-2 cursor-pointer rounded-md" v-if="!image.is_primary" v-on:click="makePrimaryImage(image.id)">Make primary</h1>
+          <h1 class="text-red-500 font-bold text-center bg-white-matcha px-4 py-2 cursor-pointer rounded-md" v-on:click="deleteImage(image.id)">Delete</h1>
+          <h1 class="text-green-500 font-bold text-center bg-white-matcha px-4 py-2 mt-2 cursor-pointer rounded-md" v-if="!image.is_primary" v-on:click="makePrimaryImage(image.id)">Make primary</h1>
         </div>
       </div>
     </div>
