0b0a9bd406db9623b87874ed1b590670edaca8be
Larry <lauris.skraucis@gmail.com>
2020-10-14 08:56:42 +0300

    image settings buttons lighter font

--- frontend/src/components/app/settings/ProfileImage.vue
+++ frontend/src/components/app/settings/ProfileImage.vue
@@ -6,8 +6,8 @@
       <div class="absolute top-0 left-0 z-20 m-5">
         <img src="../../../assets/dots.png" tabindex="2" @focusout="showImageSettings = false" v-on:click="showImageSettings = !showImageSettings " class="outline-none cursor-pointer h-3">
         <div v-if="showImageSettings" class="z-30 mt-4">
-          <h1 class="text-red-500 font-bold text-center bg-white-matcha px-4 py-2 cursor-pointer rounded-md" v-on:click="deleteImage(image.id)">Delete</h1>
-          <h1 class="text-green-500 font-bold text-center bg-white-matcha px-4 py-2 mt-2 cursor-pointer rounded-md" v-if="!image.is_primary" v-on:click="makePrimaryImage(image.id)">Make primary</h1>
+          <h1 class="text-red-500 text-center bg-white-matcha px-4 py-2 cursor-pointer rounded-md" v-on:click="deleteImage(image.id)">Delete</h1>
+          <h1 class="text-green-500 text-center bg-white-matcha px-4 py-2 mt-2 cursor-pointer rounded-md" v-if="!image.is_primary" v-on:click="makePrimaryImage(image.id)">Make primary</h1>
         </div>
       </div>
     </div>
