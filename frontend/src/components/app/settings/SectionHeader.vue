6fdd564e520aa0650d3bba90373cedc7f7a9b45d
Larry <lauris.skraucis@gmail.com>
2020-10-14 08:45:02 +0300

    more spacious account card and section header purple

--- frontend/src/components/app/settings/SectionHeader.vue
+++ frontend/src/components/app/settings/SectionHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex max-w-sm justify-between items-center w-full my-4">
-    <h1 class="text-3xl text-gray-matcha capitalize">{{ name }}</h1>
+    <h1 class="text-3xl text-purple-400 capitalize">{{ name }}</h1>
     <div class="md:hidden sort-button rounded-md text-lg w-12">
       <h1 class="noSelect">←</h1>
     </div>
