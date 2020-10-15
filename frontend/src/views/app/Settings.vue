3da1e43d919660ef9339ec73a9d071debd7a9402
Larry <lauris.skraucis@gmail.com>
2020-10-15 10:01:02 +0300

    fetch user data when going to settings

--- frontend/src/views/app/Settings.vue
+++ frontend/src/views/app/Settings.vue
@@ -276,6 +276,8 @@ export default {
       this.show = 'profile';
     }
     console.log(this.$store.getters.getLoggedInUser);
+    const user = await this.$http.get(`/users/${this.$store.getters.getLoggedInUser.id}`);
+    await this.$store.dispatch('login', user.data);
     const tags = this.$store.getters.getLoggedInUser.tags;
     for (let i = 0; i < tags.length; i += 1) {
       this.userInterests.push(tags[i].name);
