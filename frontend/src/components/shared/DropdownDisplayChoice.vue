a51cf70db835ec979ab0ff48dddf964e39cb39e0
Larry <lauris.skraucis@gmail.com>
2020-10-19 08:53:42 +0200

    dropdown add triangle pointing down icon

--- frontend/src/components/shared/DropdownDisplayChoice.vue
+++ frontend/src/components/shared/DropdownDisplayChoice.vue
@@ -2,7 +2,7 @@
   <!-- eslint-disable max-len -->
   <div class="focus:outline-none sm:flex-none" @focusout="close" tabindex="0">
     <div v-bind:class="{'settings-button-choice': true, 'border-gray-matcha': !closed}" @click="toggle">
-      <h1 v-bind:class="{ 'opacity-50': closed, 'noSelect': true, 'capitalize': true }">{{ currentOption }}</h1>
+      <h1 v-bind:class="{ 'opacity-50': closed, 'noSelect': true, 'capitalize': true }">{{ currentOption }}<span class="ml-2">▼</span></h1>
     </div>
     <div v-bind:class="{'sort-dropdown': true, 'z-10': true , 'hidden': closed, 'h-auto': options.length < 5}">
       <h1 v-for="(option, index) in options" :key="option + index + option"
