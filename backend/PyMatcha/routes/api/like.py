86757fcf2b06bfe0ab2fcca362d49fceeafb6b3f
Jules Lasne <jules.lasne@gmail.com>
2020-10-23 18:15:16 +0200

    Added notifications for likes, messages and views

--- backend/PyMatcha/routes/api/like.py
+++ backend/PyMatcha/routes/api/like.py
@@ -7,6 +7,7 @@ from flask_jwt_extended import current_user
 from flask_jwt_extended import jwt_required
 from PyMatcha.models.like import Like
 from PyMatcha.models.match import Match
+from PyMatcha.models.notification import Notification
 from PyMatcha.models.user import get_user
 from PyMatcha.utils.decorators import validate_params
 from PyMatcha.utils.errors import BadRequestError
@@ -53,11 +54,35 @@ def like_profile(uid):
 
     if u.already_likes(current_user.id):
         Match.create(user_1=current_user.id, user_2=u.id)
+        Notification.create(
+            user_id=u.id, content=f"{u.first_name} liked you! Go check them out!", type="like", link_to=f"users/{u.id}"
+        )
+        Notification.create(
+            user_id=u.id,
+            content=f"You and {current_user.first_name} matched!",
+            type="match",
+            link_to=f"conversation/{current_user.id}",
+        )
+        Notification.create(
+            user_id=current_user.id,
+            content=f"You and {u.first_name} matched!",
+            type="match",
+            link_to=f"conversation/{u.id}",
+        )
         return Success("It's a match !")
 
     if is_superlike:
+        Notification.create(
+            user_id=u.id,
+            content=f"{u.first_name} superliked you 😏! Go check them out!",
+            type="superlike",
+            link_to=f"users/{u.id}",
+        )
         return Success("Superliked user.")
     else:
+        Notification.create(
+            user_id=u.id, content=f"{u.first_name} liked you! Go check them out!", type="like", link_to=f"users/{u.id}"
+        )
         return Success("Liked user.")
 
 
@@ -73,4 +98,7 @@ def unlike_profile(uid):
     if not current_user.already_likes(u.id):
         raise BadRequestError("You never liked this person in the first place.")
     Like.get_multi(liked_id=u.id, liker_id=current_user.id).delete()
+
+    Notification.create(user_id=u.id, content=f"{u.first_name} unliked you.", type="unlike", link_to=None)
+
     return Success(f"Unliked user {u.id}.")
--- backend/PyMatcha/routes/api/profile/view.py
+++ backend/PyMatcha/routes/api/profile/view.py
@@ -19,8 +19,10 @@
 from flask import Blueprint
 from flask_jwt_extended import current_user
 from flask_jwt_extended import jwt_required
+from PyMatcha.models.notification import Notification
 from PyMatcha.models.user import get_user
 from PyMatcha.models.view import View
+from PyMatcha.utils.errors import BadRequestError
 from PyMatcha.utils.errors import NotFoundError
 from PyMatcha.utils.match_score import _get_distance
 from PyMatcha.utils.success import SuccessOutput
@@ -36,11 +38,19 @@ def view_profile(uid):
     except NotFoundError:
         raise NotFoundError(f"User {uid} not found.")
 
-    if current_user.id != u.id:
-        View.create(profile_id=u.id, viewer_id=current_user.id)
+    if current_user.id == u.id:
+        raise BadRequestError("Cannot view yoursel")
+    View.create(profile_id=u.id, viewer_id=current_user.id)
 
     user_dict = u.to_dict()
     # TODO: Update swagger with this
     user_dict["distance"] = _get_distance(current_user.geohash, u.geohash)
+
+    Notification.create(
+        user_id=u.id,
+        content=f"{u.first_name} just viewed your profile! Go check their profile out!",
+        type="view",
+        link_to=f"users/{u.id}",
+    )
 
     return SuccessOutput("profile", user_dict)
