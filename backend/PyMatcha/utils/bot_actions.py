fd4ed7fca52a8a7d718834a1ae2ea23ff18ca103
Jules Lasne <jules.lasne@gmail.com>
2020-10-23 16:46:48 +0200

    Updated decisions to be a flow instead of random

--- backend/PyMatcha/utils/bot_actions.py
+++ backend/PyMatcha/utils/bot_actions.py
@@ -1,6 +1,8 @@
 import json
 import logging
 from random import choice
+from random import choices
+from random import randrange
 
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
@@ -18,7 +20,7 @@ from PyMatcha.utils.static import BOT_CONV_OPENERS
 # TODO: DO superlikes
 
 
-def _bot_response(bot_name, user_input):
+def _prepare_chatbot(bot_name):
     logging.debug(f"Starting chatbot with name {bot_name}")
     chatbot = ChatBot(
         bot_name,
@@ -34,7 +36,7 @@ def _bot_response(bot_name, user_input):
         "chatterbot.corpus.english.humor",
         "PyMatcha.utils.dating",
     )
-    return chatbot.get_response(user_input)
+    return chatbot
 
 
 def _get_recommendations(bot_user: User):
@@ -47,8 +49,7 @@ def _get_recommendations(bot_user: User):
     return json.loads(recommendations)
 
 
-def botaction_like(bot_user: User):
-    recommendations = _get_recommendations(bot_user)
+def _botaction_like(bot_user: User, recommendations):
     liked_ids = [like.liked_id for like in bot_user.get_likes_sent()]
     for user in recommendations:
         if user["id"] in liked_ids:
@@ -74,8 +75,7 @@ def botaction_unlike(bot_user: User):
     Like.get_multi(liker_id=bot_user.id, liked_id=id_to_unlike).delete()
 
 
-def botaction_view(bot_user: User):
-    recommendations = _get_recommendations(bot_user)
+def _botaction_view(bot_user: User, recommendations):
     try:
         user_to_view = choice(recommendations)
     except IndexError:
@@ -83,7 +83,7 @@ def botaction_view(bot_user: User):
     View.create(profile_id=user_to_view["id"], viewer_id=bot_user.id)
 
 
-def botaction_message_new_conversation(bot_user: User):
+def _botaction_message_new_conversation(bot_user: User):
     matches = bot_user.get_matches()
     unopened_matches = []
     for match in matches:
@@ -105,7 +105,7 @@ def botaction_message_new_conversation(bot_user: User):
     bot_user.send_message(to_id=other_user, content=choice(BOT_CONV_OPENERS))
 
 
-def botaction_respond_to_unread(bot_user: User):
+def _botaction_respond_to_unread(bot_user: User, chatbot):
     last_message_list = bot_user.get_conversation_list()
 
     unread_last_messages = []
@@ -116,11 +116,11 @@ def botaction_respond_to_unread(bot_user: User):
         message_to_reply = choice(unread_last_messages)
     except IndexError:
         return
-    bot_reply = _bot_response(bot_user.username, message_to_reply.content)
+    bot_reply = chatbot.get_response(message_to_reply.content)
     bot_user.send_message(to_id=message_to_reply.from_id, content=bot_reply)
 
 
-def botaction_send_message_over_old_one(bot_user: User):
+def _botaction_send_message_over_old_one(bot_user: User, chatbot):
     last_message_list = bot_user.get_conversation_list()
     try:
         message_to_reply = choice(last_message_list)
@@ -131,5 +131,41 @@ def botaction_send_message_over_old_one(bot_user: User):
     else:
         other_user = message_to_reply.to_id
 
-    bot_reply = _bot_response(bot_user.username, ".")
+    bot_reply = chatbot.get_response(".")
     bot_user.send_message(to_id=other_user, content=bot_reply)
+
+
+def decide_bot_action(bot_user: User):
+    recommendations = _get_recommendations(bot_user)
+
+    # The bot will first view 0 to 10 profiles
+    for _ in range(0, randrange(0, 10)):
+        _botaction_view(bot_user, recommendations)
+
+    # The bot will then like 0 to 3 profiles
+    for _ in range(0, randrange(0, 3)):
+        _botaction_like(bot_user, recommendations)
+
+    matches = bot_user.get_matches()
+    if not matches:
+        # No matches so far, no more actions to be done
+        # TODO: Add unlike
+        return
+
+    for match in matches:
+        # if match.user_1 == bot_user.id:
+        #     other_user_id = match.user_2
+        # else:
+        #     other_user_id = match.user_1
+
+        chatbot = _prepare_chatbot(bot_user.username)
+
+        message_actions = [
+            _botaction_respond_to_unread,
+            _botaction_message_new_conversation,
+            _botaction_send_message_over_old_one,
+        ]
+        selected_action = choices(message_actions, weights=[10, 9, 5], k=1)
+        if selected_action[0]:
+            # Has to have the [0] because random.choices return a list
+            selected_action[0](bot_user, chatbot)
