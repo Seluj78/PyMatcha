86ddb54ae70deffec4020544034fd4585f2cf8c5
Jules Lasne <jules.lasne@gmail.com>
2020-10-23 11:23:36 +0200

    Added base chatbot action to get a response

---
+++ backend/PyMatcha/utils/chatbot_actions.py
@@ -0,0 +1,21 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from PyMatcha.utils.static import BACKEND_ROOT
+
+
+def get_chatbot_response(bot_name, user_input):
+    chatbot = ChatBot(
+        bot_name,
+        storage_adapter="chatterbot.storage.SQLStorageAdapter",
+        database_uri=f"sqlite:///{BACKEND_ROOT}/../chatbot_database.sqlite3",
+    )
+
+    trainer = ChatterBotCorpusTrainer(chatbot, show_training_progress=False)
+    trainer.train(
+        "chatterbot.corpus.english.conversations",
+        "chatterbot.corpus.english.emotion",
+        "chatterbot.corpus.english.greetings",
+        "chatterbot.corpus.english.humor",
+        "PyMatcha.utils.dating",
+    )
+    return chatbot.get_response(user_input)
