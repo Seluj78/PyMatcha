8ae0c634b1d2bbe71d5c3da061c4660af807ddcd
Larry <lauris.skraucis@gmail.com>
2020-10-30 12:45:51 +0100

    logout undefined store and router fix

--- frontend/src/auth/logOut.js
+++ frontend/src/auth/logOut.js
@@ -1,7 +1,11 @@
 /* eslint-disable import/prefer-default-export */
 import Axios from 'axios';
+import router from '../router';
+import store from '../store';
 
 export const logOut = async () => {
+  await store.dispatch('logout');
+  await router.push('/accounts/signin');
   await Axios.post('/auth/logout', {
     access_token: localStorage.getItem(process.env.VUE_APP_ACCESS_TOKEN),
     refresh_token: localStorage.getItem(process.env.VUE_APP_REFRESH_TOKEN),
@@ -10,6 +14,4 @@ export const logOut = async () => {
   localStorage.removeItem(process.env.VUE_APP_REFRESH_TOKEN);
   localStorage.removeItem(process.env.VUE_APP_VUEX_PERSISTED_STATE);
   localStorage.removeItem(process.env.VUE_APP_SECURE_LS_METADATA);
-  await this.$store.dispatch('logout');
-  await this.$router.push('/accounts/signin');
 };
