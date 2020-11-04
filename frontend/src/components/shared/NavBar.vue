50917784786e0d4ea57b04c11029363c50322f37
Larry <lauris.skraucis@gmail.com>
2020-11-04 13:59:34 +0100

    navbar tracks current route

--- frontend/src/components/shared/NavBar.vue
+++ frontend/src/components/shared/NavBar.vue
@@ -56,8 +56,7 @@ export default {
       return this.$store.getters.getLoggedInUser;
     },
     currentRoute() {
-      console.log(this.$router.currentRoute);
-      return this.$route.name;
+      return this.$router.currentRoute.name;
     },
   },
 };
