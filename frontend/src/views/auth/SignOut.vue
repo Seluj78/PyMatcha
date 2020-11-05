20794ee6ca9548400153708769e0a2adfd9aeb5e
Larry <lauris.skraucis@gmail.com>
2020-11-05 11:42:44 +0100

    sign out loading icon

--- frontend/src/views/auth/SignOut.vue
+++ frontend/src/views/auth/SignOut.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>signing out...</div>
+  <section class="mx-auto onboarding-sub-container bg-white w-full h-screen relative">
+    <h1
+      class="onboarding-sub-container-content-heading text-5xl mx-auto leading-none text-center">
+      Signing<br>out
+    </h1>
+    <img class="mt-4 h-32 mx-auto" src='../../assets/loading.svg'>
+  </section>
 </template>
 
 <script>
