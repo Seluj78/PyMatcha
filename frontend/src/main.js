4fa24dbcf62fbb15afff921c7f33dcf1e0931d40
Larry <lauris.skraucis@gmail.com>
2020-11-05 10:50:20 +0100

    check env variables before app boots

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -17,8 +17,22 @@ Vue.config.productionTip = false;
 Vue.use(http);
 Vue.use(errorMessenger);
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+function envHasNecessaryVariables() {
+  return !(!process.env.VUE_APP_BACKEND_BASE_URL
+    || !process.env.VUE_APP_ACCESS_TOKEN
+    || !process.env.VUE_APP_REFRESH_TOKEN
+    || !process.env.VUE_APP_VUEX_PERSISTED_STATE
+    || !process.env.VUE_APP_SECURE_LS_METADATA);
+}
+
+if (envHasNecessaryVariables()) {
+  new Vue({
+    router,
+    store,
+    render: (h) => h(App),
+  }).$mount('#app');
+} else {
+  console.log('Environment variables missing. Please, set VUE_APP_BACKEND_BASE_URL,'
+    + 'VUE_APP_ACCESS_TOKEN, VUE_APP_REFRESH_TOKEN, VUE_APP_VUEX_PERSISTED_STATE,'
+    + 'VUE_APP_SECURE_LS_METADATA variables in .env file of frontend folder');
+}
