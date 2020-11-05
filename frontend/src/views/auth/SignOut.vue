90ca53ba85ad4df1509748e336043e0bea18d4a3
Larry <lauris.skraucis@gmail.com>
2020-11-05 16:06:00 +0100

    revoked refresh token logout fix

--- frontend/src/views/auth/SignOut.vue
+++ frontend/src/views/auth/SignOut.vue
@@ -4,13 +4,17 @@
 
 <script>
 
+/* eslint-disable no-empty */
+
 export default {
   name: 'SignOut',
   async mounted() {
-    await this.$http.post('/auth/logout', {
-      access_token: localStorage.getItem(process.env.VUE_APP_ACCESS_TOKEN),
-      refresh_token: localStorage.getItem(process.env.VUE_APP_REFRESH_TOKEN),
-    });
+    try {
+      await this.$http.post('/auth/logout', {
+        access_token: localStorage.getItem(process.env.VUE_APP_ACCESS_TOKEN),
+        refresh_token: localStorage.getItem(process.env.VUE_APP_REFRESH_TOKEN),
+      });
+    } catch (e) {}
     localStorage.removeItem(process.env.VUE_APP_ACCESS_TOKEN);
     localStorage.removeItem(process.env.VUE_APP_REFRESH_TOKEN);
     localStorage.removeItem(process.env.VUE_APP_VUEX_PERSISTED_STATE);
