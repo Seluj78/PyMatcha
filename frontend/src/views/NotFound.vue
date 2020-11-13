11d4221b7ef69c6de47f755a874b7350d855968b
Larry <lauris.skraucis@gmail.com>
2020-11-13 17:35:19 +0100

    404 page back button

--- frontend/src/views/NotFound.vue
+++ frontend/src/views/NotFound.vue
@@ -1,5 +1,7 @@
 <template>
+  <!-- eslint-disable max-len -->
   <div class="sm:mx-16 lg:mx-32 mt-32">
     <h1 class="text-2xl text-center mx-auto">404 page not found</h1>
+    <h1 @click="$router.go(-1)" class="text-center text-center mx-auto cursor-pointer text-blue-500 mt-2">Back</h1>
   </div>
 </template>
