f6f5e6249d64372551e2e768a730b16ceaeae245
Larry <lauris.skraucis@gmail.com>
2020-11-14 11:36:52 +0100

    jules and lauris footer links

--- frontend/src/components/shared/Footer.vue
+++ frontend/src/components/shared/Footer.vue
@@ -1,14 +1,24 @@
 <template>
   <!-- eslint-disable max-len -->
   <!-- eslint-disable vue/no-deprecated-v-on-native-modifier -->
-  <div class="w-full text-center my-8">
+  <div class="w-full text-center py-12">
     <div class="flex justify-center">
-      <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/laurisskraucis/"><img src="../../assets/home/instagram.png" class="opacity-50 hover:opacity-75 w-8 m-1 sm:mx-2"></a>
-      <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/laurisskraucis/"><img src="../../assets/home/facebook.png" class="opacity-50 hover:opacity-75 w-8 m-1 sm:mx-2"></a>
-      <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/laurisskraucis/"><img src="../../assets/home/twitter.png" class="opacity-50 hover:opacity-75 w-8 m-1 sm:mx-2"></a>
-      <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/laurisskraucis/"><img src="../../assets/home/linkedin.png" class="opacity-50 hover:opacity-75 w-8 m-1 sm:mx-2"></a>
+      <div class="flex flex-col mr-4">
+        <div class="flex">
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/Seluj78" class="outline-none"><img src="../../assets/home/github.png" class="opacity-50 hover:opacity-75 w-8 m-1 sm:mx-2"></a>
+          <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/jules-lasne/" class="outline-none"><img src="../../assets/home/linkedin.png" class="opacity-50 hover:opacity-75 w-8 m-1 sm:mx-2"></a>
+        </div>
+        <h1 class="font-semibold mt-1">Jules</h1>
+      </div>
+      <div class="flex flex-col ml-4">
+        <div class="flex">
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/supalarry" class="outline-none"><img src="../../assets/home/github.png" class="opacity-50 hover:opacity-75 w-8 m-1 sm:mx-2"></a>
+          <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/laurisskraucis/" class="outline-none"><img src="../../assets/home/linkedin.png" class="opacity-50 hover:opacity-75 w-8 m-1 sm:mx-2"></a>
+        </div>
+        <h1 class="font-semibold mt-1">Lauris</h1>
+      </div>
     </div>
-    <h1 class="mt-4 opacity-50 font-semibold">Ⓡ Matcha 2020</h1>
+    <h1 class="mt-6 opacity-50 font-semibold">Ⓡ Matcha 2020</h1>
   </div>
 </template>
 
