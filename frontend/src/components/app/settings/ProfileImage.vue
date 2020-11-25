6ab4b5f1358d92d873ae5db94bc106a423fb94e6
Larry <lauris.skraucis@gmail.com>
2020-11-25 08:27:38 +0100

    fix settings images buttons chrome

--- frontend/src/components/app/settings/ProfileImage.vue
+++ frontend/src/components/app/settings/ProfileImage.vue
@@ -4,7 +4,7 @@
     <div class="relative overflow-hidden bg-transparent rounded-md w-full" style="padding-bottom: 70%">
       <img v-bind:src="image.link" class="absolute object-cover w-full h-full rounded-md">
       <div class="absolute top-0 left-0 z-20 m-5">
-        <img src="../../../assets/dots.png" tabindex="2" @focusout="showImageSettings = false" v-on:click="showImageSettings = !showImageSettings " class="outline-none cursor-pointer h-3">
+        <img src="../../../assets/dots.png" v-on:click="showImageSettings = !showImageSettings " class="outline-none cursor-pointer h-3">
         <div v-if="showImageSettings" class="z-30 mt-4">
           <h1 class="text-red-500 text-center bg-white-matcha px-4 py-2 cursor-pointer rounded-md" v-on:click="deleteImage(image.id)">Delete</h1>
           <h1 class="text-green-500 text-center bg-white-matcha px-4 py-2 mt-2 cursor-pointer rounded-md" v-if="!image.is_primary" v-on:click="makePrimaryImage(image.id)">Make primary</h1>
